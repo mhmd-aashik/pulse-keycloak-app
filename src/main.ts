17036cbc6b1fca390928e7513d1938e8f742d8b7
Mohammed Aashik <aashikdevelop@gmail.com>
2026-06-24 09:01:29 +0530

    feat: enable CORS and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
